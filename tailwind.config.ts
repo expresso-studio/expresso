0719bdd6c3dd3212611c6bc2cdd63bfc35121d94
peicasey <peicasey@gmail.com>
2025-02-13 10:49:58 -0600

    add custom tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +12,33 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
+        lightCream: "#F6F1EE",
+        lightCoffee: "#936648",
+        lightLatte: "#D5A585",
+        lightCaramel: "#C06C35",
+        lightBurnt: "#100B09",
+        darkCream: "#F6F1EF",
+        darkLatte: "#B78A6C",
+        darkCoffee: "#7A4A2A",
+        darkCaramel: "CA773F",
+        darkBurnt: "#110C09",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
