5b8fbc6869f5fc60da973abf6014143610ca6681
Kevtom2003 <kevtom2003@gmail.com>
2025-02-17 16:51:50 -0600

    giving up on making oauth perfect for now

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -15,53 +15,54 @@ export default function Page() {
   if (isLoading) {
     return <div>Loading ...</div>;
   }
-  console.log(user);
-  return (
-    <PageFormat breadCrumbs={[]}>
-      <Heading1 id="greeting">
-        Hi {user?.nickname}<span className="text-lightCoffee dark:text-darkCoffee"></span>
-        !
-      <LogoutButton />
-      </Heading1>
-      <div className="mt-4 mb-16 flex flex-col md:flex-row gap-16">
-        <div className="flex flex-col gap-16 w-full">
-          <Section
-            id="evaluate"
-            title="Jump on in!"
-            className="sm:min-w-[300px] bg-lightCoffee dark:bg-darkCoffee group cursor-pointer"
-          >
-            <Link href={"/dashboard/evaluate"} className=" overflow-hidden">
-              <div className="absolute z-20 flex flex-col gap-2 w-full">
-                <span className="text-6xl md:text-[5vw] font-black text-white uppercase group-hover:translate-x-1 duration-200">
-                  Evaluate
-                </span>
-                <span className="text-2xl lg:text-4xl italic text-white group-hover:translate-x-2 duration-500">
-                  a new presentation
-                </span>
-              </div>
-              <div className="h-[30vh] flex flex-col items-end justify-end">
-                <Image
-                  src={"./teapot.svg"}
-                  alt={"a teapot"}
-                  width="500"
-                  height="500"
-                  className="w-[80%] max-w-[450px] absolute translate-x-12 translate-y-12 scale-x-[-1] z-10 group-hover:rotate-6 group-hover:scale-y-[1.05] ease-out duration-200"
-                />
-                <div className="flex flex-row justify-end items-end w-full h-full">
-                  <div className="bg-[#ac795d] dark:bg-[#9c6446] w-[80%] h-[80%] z-0 rounded-lg group-hover:scale-105 duration-500"></div>
+  if(isAuthenticated && user){
+    return (
+      <PageFormat breadCrumbs={[]}>
+        <Heading1 id="greeting">
+          Hi {user.nickname}<span className="text-lightCoffee dark:text-darkCoffee"></span>
+          !
+        <LogoutButton />
+        </Heading1>
+        <div className="mt-4 mb-16 flex flex-col md:flex-row gap-16">
+          <div className="flex flex-col gap-16 w-full">
+            <Section
+              id="evaluate"
+              title="Jump on in!"
+              className="sm:min-w-[300px] bg-lightCoffee dark:bg-darkCoffee group cursor-pointer"
+            >
+              <Link href={"/dashboard/evaluate"} className=" overflow-hidden">
+                <div className="absolute z-20 flex flex-col gap-2 w-full">
+                  <span className="text-6xl md:text-[5vw] font-black text-white uppercase group-hover:translate-x-1 duration-200">
+                    Evaluate
+                  </span>
+                  <span className="text-2xl lg:text-4xl italic text-white group-hover:translate-x-2 duration-500">
+                    a new presentation
+                  </span>
                 </div>
-              </div>
-            </Link>
-          </Section>
-          <Section id="learning" title="Learning">
-            <>aasdf</>
-          </Section>
+                <div className="h-[30vh] flex flex-col items-end justify-end">
+                  <Image
+                    src={"./teapot.svg"}
+                    alt={"a teapot"}
+                    width="500"
+                    height="500"
+                    className="w-[80%] max-w-[450px] absolute translate-x-12 translate-y-12 scale-x-[-1] z-10 group-hover:rotate-6 group-hover:scale-y-[1.05] ease-out duration-200"
+                  />
+                  <div className="flex flex-row justify-end items-end w-full h-full">
+                    <div className="bg-[#ac795d] dark:bg-[#9c6446] w-[80%] h-[80%] z-0 rounded-lg group-hover:scale-105 duration-500"></div>
+                  </div>
+                </div>
+              </Link>
+            </Section>
+            <Section id="learning" title="Learning">
+              <>aasdf</>
+            </Section>
+          </div>
+          <div className="flex flex-col gap-16 w-full">
+            <PreviousPresentationsSection />
+            <ProgressSection />
+          </div>
         </div>
-        <div className="flex flex-col gap-16 w-full">
-          <PreviousPresentationsSection />
-          <ProgressSection />
-        </div>
-      </div>
-    </PageFormat>
-  );
+      </PageFormat>
+    );
+  }
 }
