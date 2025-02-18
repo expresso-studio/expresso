8cb7dd9649c1f1f93b011f8effec81ac01081a04
RahifMansoor <rahif2004@gmail.com>
2025-02-17 19:26:48 -0600

    added new visualization functionality

---
+++ app/evaluate/page.tsx
@@ -0,0 +1,17 @@
+/**
+ * Main page component that renders the GestureAnalyzer component.
+ * This component is wrapped with Suspense to handle the dynamic imports
+ * and prevent hydration errors.
+ */
+import { Suspense } from 'react';
+import GestureAnalyzer from './GestureAnalyzer';
+
+export default function Page() {
+  return (
+    <div className="relative">
+      <Suspense fallback={<div>Loading...</div>}>
+        <GestureAnalyzer />
+      </Suspense>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-/**
- * Main page component that renders the GestureAnalyzer component.
- * This component is wrapped with Suspense to handle the dynamic imports
- * and prevent hydration errors.
- */
-import { Suspense } from 'react';
-import GestureAnalyzer from '@/evaluate/GestureAnalyzer';
+
 
 export default function Page() {
   return (
-    <div className="relative">
-      <Suspense fallback={<div>Loading...</div>}>
-        <GestureAnalyzer />
-      </Suspense>
-    </div>
+    <main>
+      hello
+    </main>
   );
 }
