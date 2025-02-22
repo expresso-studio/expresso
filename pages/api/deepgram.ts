43d49882fa04d564b49032cb0692fe08f79fed63
Kevtom2003 <kevtom2003@gmail.com>
2025-02-22 16:50:53 -0600

    backend fully merged

--- pages/api/deepgram.ts
+++ pages/api/deepgram.ts
@@ -1,14 +1,5 @@
-import { startWebSocketServer } from '../../lib/start-deepgram-websocket';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// Start the WebSocket server when this file is imported.
-const wsServer = startWebSocketServer();
-
-// Export a default function to handle API requests.
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!wsServer) {
-    return res.status(500).json({ message: 'WebSocket server is not running' });
-  }
-
   return res.status(200).json({ message: 'WebSocket server is running' });
 }
