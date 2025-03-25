491f03248677bc05149e5416c9a9d4531f3d0ca9
peicasey <peicasey@gmail.com>
2025-03-24 23:27:43 -0500

    fix build issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import LoginButton from "../components/login-button";
 import FooterWave from "@/components/ui/footer-wave";
 import Image from "next/image";
-import { ArrowRight } from "lucide-react";
 
 export default function Page() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow flex items-center justify-center">
-        <div className="flex items-center grid grid-cols-2 gap-8 max-w-4xl">
+        <div className="items-center grid grid-cols-2 gap-8 max-w-4xl">
           <div className="p-6 rounded-lg">
             <Image
               src={"/expresso.png"}
